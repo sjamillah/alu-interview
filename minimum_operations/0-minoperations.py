b568da6affc587b24a65c9d8b67a23ca3c55a361
sjamillah <j.ssozi@alustudent.com>
2024-01-20 12:24:58 +0000

    minoperations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script to calculate the fewest number of operations to result in n H chars"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    x = 2
+    result = 0
+
+    while x <= n:
+        if n % x == 0:
+            result += 1
+            n = n / x
+        else:
+            i += 1
+    return result
