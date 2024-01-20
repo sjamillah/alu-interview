23e20bce21f895079ee9591968c084e0e24edcac
sjamillah <j.ssozi@alustudent.com>
2024-01-20 12:45:08 +0000

    fixed pep8 errors

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,15 +2,15 @@
 """Script to calculate the fewest number of operations to result in n H chars"""
 
 def minOperations(n):
+    """Minimum Operations"""
     if n <= 1:
         return 0
 
     x = 2
     result = 0
-
     while x <= n:
         if n % x == 0:
-            result += 1
+            result += x
             n = n / x
         else:
             x += 1
