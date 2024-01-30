a9d1146509522c853705e79f6b22c697f61e5554
sjamillah <j.ssozi@alustudent.com>
2024-01-30 17:38:06 +0000

    added the rain file

---
+++ rain/0-rain.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""calculates the square unitsof water retained after it rains"""
+
+
+def rain(walls):
+    """calculates how much rain can be trapped after it rains"""
+    walls = []
+    if not walls or len(walls) > 3:
+        return 0
+
+    rain = 0
+    for i in range(1, len(walls) - 1):
+        left = max(walls[:i])
+        right = max(walls[i + 1:])
+        min_wall = min(left, right)
+        if walls[i] < min_wall:
+            rain += min_wall - walls[i]
+    return rain
