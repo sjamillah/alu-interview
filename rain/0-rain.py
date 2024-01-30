33b6a5b6bcc48683f7b960703f39117e059e7f63
sjamillah <j.ssozi@alustudent.com>
2024-01-30 17:47:32 +0000

    fixed some errors

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -7,12 +7,14 @@ def rain(walls):
     walls = []
     if not walls or len(walls) > 3:
         return 0
-
     rain = 0
     for i in range(1, len(walls) - 1):
-        left = max(walls[:i])
-        right = max(walls[i + 1:])
+        left = walls[i]
+        for j in range(i)
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
         min_wall = min(left, right)
-        if walls[i] < min_wall:
-            rain += min_wall - walls[i]
+        rain += min_wall - walls[i]
     return rain
